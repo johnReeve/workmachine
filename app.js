6b329cfd002b061d95581370b4f495a4bf67fede
John Reeve <John.reeve.media@gmail.com>
2014-02-26 00:24:21 -0600

    favicon

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.compress());
-app.use(express.favicon());
+app.use(express.favicon('public/favicon.ico'));
 app.use(express.logger('dev'));
 app.use(express.cookieParser());
 app.use(express.json());
